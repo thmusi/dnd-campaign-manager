19b3c9aae1875bdb47022985d6a9be2a0bf7a011
thmusi <127438712+thmusi@users.noreply.github.com>
2025-03-01 00:18:49 +0100

    retour à la ligne after load_dotenv()

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ DEFAULT_CART_STRUCTURE = {
 
 # Load environment variables
 load_dotenv()
+
 GOOGLE_DRIVE_CREDENTIALS_PATH = os.getenv("GOOGLE_DRIVE_CREDENTIALS_PATH", "/etc/secrets/GOOGLE_DRIVE_CREDENTIALS")
 
 if os.path.exists(GOOGLE_DRIVE_CREDENTIALS_PATH):
