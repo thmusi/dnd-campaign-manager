9fe57925806c85f21cb6d18ad1053fb92786a562
thmusi <127438712+thmusi@users.noreply.github.com>
2025-03-10 09:05:17 +0100

    Update embedding_management.py

--- embedding_management.py
+++ embedding_management.py
@@ -6,6 +6,8 @@ import subprocess
 import yaml
 from pathlib import Path
 import openai
+import pandas as pd
+
 
 CHROMA_DB_PATH = "chroma_db/"
 CONFIG_FILE = "config.yaml"  # Now stored in your app's GitHub repo
